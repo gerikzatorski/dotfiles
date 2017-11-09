6fe0f59e541963bcb242f55a77299efc889b91ce
Gerik Zatorski <gerikzatorski@gmail.com>
2017-11-08 22:57:12 -0600

    added python startup file

---
+++ .startup.py
@@ -0,0 +1,12 @@
+# define PYTHONSTARTUP=~/.startup.py
+print("(.startup.py)")
+import datetime, os, pprint, re, sys, time
+print("(imported datetime, os, pprint, re, sys, time)")
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+print("(imported numpy and matplotlib)")
+
+
+pp = pprint.pprint
