88d907b57e4bacc715efa6146c16851f90de4946
gerik <gerikzatorski@gmail.com>
2017-12-14 01:36:54 -0600

    new

--- install.bash
+++ install.bash
@@ -8,15 +8,18 @@ sudo dpkg -i google-chrome*.deb
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
 
-# 
+# general packages
+sudo apt-get install -y vlc git xclip cmake
+
+# emacs stuff
 sudo apt install emacs25
+sudo apt-get install -y clang virtualenv
 
-# packages
-sudo apt-get install -y vlc git python-pip
-
+# configs
 git clone https://github.com/gerikzatorski/.emacs.d
 git clone https://github.com/gerikzatorski/dotfiles
 rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc
 
-
+# python stuff
+sudo apt-get install -y python-pip python-numpy python-scipy python-matplotlib
